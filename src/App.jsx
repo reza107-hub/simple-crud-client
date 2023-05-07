f0c0856d1175c74854c70f72fe905cc522df37d4
reza107-hub <rashidaminur104@gmail.com>
2023-05-07 19:15:41 +0600

    installing react router and post in mongodb

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=> console.log(data))
+  };
+  return (
+    <>
+      <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+}
+
+export default App;
